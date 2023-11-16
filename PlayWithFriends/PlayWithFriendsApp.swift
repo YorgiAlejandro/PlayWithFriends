e25a86739fb2d0cf617356e85eec3ec2f1eb3a18
YorgiAlejandro <yorgidelriomarquez@gmail.com>
2023-11-16 10:17:22 -0500

    Initial Commit

---
+++ PlayWithFriends/PlayWithFriendsApp.swift
@@ -0,0 +1,20 @@
+//
+//  PlayWithFriendsApp.swift
+//  PlayWithFriends
+//
+//  Created by Yorgi Del Rio on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct PlayWithFriendsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
